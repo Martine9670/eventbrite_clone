2d185b16da8360d15b3583ef3de35bbf75567680
Martine9670 <pinnamartine@free.fr>
2025-07-22 10:32:05 +0200

    add age minimum

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,11 +7,20 @@ class User < ApplicationRecord
   has_many :attended_events, through: :attendances, source: :event
 
   validates :birthdate, presence: true
-
+  validate :must_be_at_least_18
 
   after_create :send_welcome_email
 
   def send_welcome_email
     UserMailer.welcome_email(self).deliver_now
+  end
+private
+
+  def must_be_at_least_18
+    return if birthdate.blank?
+
+    if birthdate > 18.years.ago.to_date
+      errors.add(:birthdate, "Tu dois avoir au moins 18 ans pour t'inscrire.")
+    end
   end
 end
