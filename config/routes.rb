86eabe5fd2897a44a49db5e31c7885c1687998cc
Martine9670 <pinnamartine@free.fr>
2025-07-23 10:24:40 +0200

    add pwa

--- config/routes.rb
+++ config/routes.rb
@@ -16,4 +16,7 @@ Rails.application.routes.draw do
 
   # Test de santé Rails
   get "up" => "rails/health#show", as: :rails_health_check
+
+  get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
+  get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 end
