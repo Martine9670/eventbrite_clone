b89a6734f63d4fc1a1c461f3389c0033a31f86a1
Martine9670 <pinnamartine@free.fr>
2025-08-05 12:09:37 +0200

    add profile avatar

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_one_attached :avatar
   has_many :events, dependent: :destroy
   has_many :attendances, dependent: :destroy
   has_many :attended_events, through: :attendances, source: :event
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,3 +5,9 @@ Rails.application.config.assets.version = "1.0"
 
 # Add additional assets to the asset load path.
 # Rails.application.config.assets.paths << Emoji.images_path
+# config/initializers/assets.rb
+
+Rails.application.config.assets.precompile += Dir[Rails.root.join("app/assets/images/**/*")].map do |path|
+  Pathname.new(path).relative_path_from(Rails.root.join("app/assets/images")).to_s
+end
+
